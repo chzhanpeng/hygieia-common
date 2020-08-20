1738a4004e16df1a65e3f269277b7e12a2c8139c
rvema <ragha.vema@capitalone.com>
2020-08-20 01:20:36 -0400

    sync from hygieia-core

---
+++ src/main/java/com/capitalone/dashboard/testutil/FongoConfig.java
@@ -0,0 +1,19 @@
+package com.capitalone.dashboard.testutil;
+
+import com.capitalone.dashboard.config.MongoConfig;
+import com.github.fakemongo.Fongo;
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+
+public class FongoConfig extends MongoConfig {
+
+    @Bean
+    public MongoClient mongo()  {
+        return new Fongo(getDatabaseName()).getMongo();
+    }
+    
+    @Override
+    protected String getDatabaseName() {
+        return "test-db";
+    }
+}
